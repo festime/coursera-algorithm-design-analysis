734e8ecbfe6dada00c0ae3cc04031486e5f77d75
Yulin Chen <gloverock2@gmail.com>
2016-08-17 16:24:35 +0800

    Fix the implementation of heap's insert and delete
    一般化 heap 可裝的物件型態，不再只侷限於 Int
    改變這部分主要是因為要用於 Dijkstra's algorithm
    heap 要能裝其他類別的物件
    只要這個類別是繼承自 Ordered[T] 即可
    繼承自 Ordered[T] 這個抽象類別
    會被要求實作一些用於比較、排序的方法
    
    其實修改是跟 Dijkstra's algorithm 的實作同步進行
    但是資料結構一改用 heap 後爆發很多錯誤
    Dijkstra's algorithm 第三個測資 size 不小
    有些問題是在這個大測資才發生
    一直用 println 和在可疑的地方放 assert
    肉眼觀察 debug 不是辦法
    
    從測資發現原來找 child index 的邏輯是錯的
    又發現 heap 的 insert, delete 裡面的
    bubble up, bubble down 有問題
    
    決定好好的把可能有問題的情況想清楚後寫測試
    來捕捉目前 heap 實作中的邏輯錯誤
    再修改實作想辦法通過測試
    
    把能想到的 heap 的一些邊界情況測試過之後
    再跑 Dijkstra's algorithm 的測試
    發現 delete 操作還有一個沒考慮清楚的邊界情況
    新增 heap 這部分的邊界測試
    修到過
    終於 Dijkstra's algorithm 的測試也過了 :)

--- Heap/src/main/scala/heap/Heap.scala
+++ Heap/src/main/scala/heap/Heap.scala
@@ -2,83 +2,116 @@ package heap
 
 import collection.mutable.ArrayBuffer
 
-class Heap {
-  private val heapRepresentation = ArrayBuffer[Int]()
-  private val positions = collection.mutable.Map[Int, ArrayBuffer[Int]]()
+class Heap[T <% Ordered[T]] {
+  private val heapRepresentation = ArrayBuffer[T]()
+  private val positions = collection.mutable.Map[T, ArrayBuffer[Int]]()
   private val IndexOfMinValue = 0
 
-  def extractMin(): Int = {
+  def extractMin(): T = {
     val min = heapRepresentation(IndexOfMinValue)
 
     swap(IndexOfMinValue, lastIndex)
     removeLastElement()
     bubbleDown(IndexOfMinValue)
 
+    try {
+      assert(heapRepresentation.forall(e => e >= min))
+    } catch {
+      case e:java.lang.AssertionError => {
+        println("======================")
+        println("min = " + min)
+        heapRepresentation.filter(e => e < min).foreach { println }
+        println("======================")
+      }
+    }
     return min
   }
 
-  def content = heapRepresentation
+  def toArrayBuffer = heapRepresentation
 
-  def delete(n: Int): Option[Int] = {
-    if (positions.getOrElse(n, ArrayBuffer[Int]()).isEmpty) {
+  def delete(n: T): Option[T] = {
+    if (positions.getOrElse(n, ArrayBuffer[T]()).isEmpty) {
       return None
 
     } else {
-      val position = positions(n).head
+      val position = positions(n).head // potential error here
 
       swap(position, lastIndex)
-      removeLastElement()
-      bubbleDown(position)
 
-      return Some(n)
+      val count = heapRepresentation.count(_ == n)
+      val result = removeLastElement()
+
+      if (position > lastIndex) {
+        return Some(result)
+      }
+
+      // bubble down
+      if (rightChildIndex(position) < heapRepresentation.length &&
+          (heapRepresentation(position) > heapRepresentation(leftChildIndex(position)) ||
+          (heapRepresentation(position) > heapRepresentation(rightChildIndex(position))))) {
+        bubbleDown(position)
+
+      } else if (leftChildIndex(position) < heapRepresentation.length &&
+               heapRepresentation(position) > heapRepresentation(leftChildIndex(position))) {
+        bubbleDown(position)
+
+      } else {
+        bubbleUp(position)
+      }
+
+      assert(heapRepresentation.count(_ == n) == count - 1)
+
+      return Some(result)
     }
   }
 
-  def insert(n: Int) = {
+  def insert(n: T) = {
     append(n)
-
-    var nodeIndex = lastIndex
-
-    def parentIndex = (nodeIndex - 1) / 2
-    def smallerThanParent = heapRepresentation(nodeIndex) < heapRepresentation(parentIndex)
-
-    while (parentIndex > IndexOfMinValue && smallerThanParent) {
-      swap(nodeIndex, parentIndex)
-      nodeIndex = parentIndex
-    }
-
-    if (
-      parentIndex == IndexOfMinValue &&
-      nodeIndex != parentIndex &&
-      heapRepresentation(nodeIndex) < heapRepresentation(parentIndex)) {
-
-      swap(nodeIndex, parentIndex)
-      nodeIndex = parentIndex
-    }
+    bubbleUp()
   }
 
   private def lastIndex: Int = heapRepresentation.length - 1
+  private def parentIndex(nodeIndex: Int): Int = (nodeIndex - 1) / 2
+  private def leftChildIndex(nodeIndex: Int): Int = (nodeIndex + 1) * 2 - 1
+  private def rightChildIndex(nodeIndex: Int): Int = (nodeIndex + 1) * 2
 
   private def bubbleDown(index: Int): Unit = {
     var nodeIndex = index
 
-    def leftChildIndex: Int = nodeIndex * 2
-    def rightChildIndex: Int = nodeIndex * 2 + 1
-
     while (true) {
-      if (
-        leftChildIndex < heapRepresentation.length &&
-        heapRepresentation(nodeIndex) > heapRepresentation(leftChildIndex)) {
+      val rightChildNodeIndex = rightChildIndex(nodeIndex)
+      val leftChildNodeIndex = leftChildIndex(nodeIndex)
 
-        swap(leftChildIndex, nodeIndex)
-        nodeIndex = leftChildIndex
+      if (rightChildNodeIndex < heapRepresentation.length) {
+        if (heapRepresentation(nodeIndex) > heapRepresentation(rightChildNodeIndex) &&
+            heapRepresentation(rightChildNodeIndex) <= heapRepresentation(leftChildNodeIndex)) {
+          swap(rightChildNodeIndex, nodeIndex)
+          assert(
+            heapRepresentation(nodeIndex) <= heapRepresentation(leftChildNodeIndex) &&
+            heapRepresentation(nodeIndex) <= heapRepresentation(rightChildNodeIndex))
+          nodeIndex = rightChildNodeIndex
 
-      } else if (
-        rightChildIndex < heapRepresentation.length &&
-        heapRepresentation(nodeIndex) > heapRepresentation(rightChildIndex)) {
+        } else if (heapRepresentation(nodeIndex) > heapRepresentation(leftChildNodeIndex) &&
+                   heapRepresentation(leftChildNodeIndex) <= heapRepresentation(rightChildNodeIndex)) {
+          swap(leftChildNodeIndex, nodeIndex)
+          assert(
+            heapRepresentation(nodeIndex) <= heapRepresentation(leftChildNodeIndex) &&
+            heapRepresentation(nodeIndex) <= heapRepresentation(rightChildNodeIndex))
+          nodeIndex = leftChildNodeIndex
 
-        swap(rightChildIndex, nodeIndex)
-        nodeIndex = rightChildIndex
+        } else {
+          return
+        }
+
+      } else if (leftChildNodeIndex < heapRepresentation.length) {
+        if (heapRepresentation(nodeIndex) > heapRepresentation(leftChildNodeIndex)) {
+          swap(leftChildNodeIndex, nodeIndex)
+          assert(heapRepresentation(nodeIndex) <= heapRepresentation(leftChildNodeIndex))
+          nodeIndex = leftChildNodeIndex
+
+        } else {
+          return
+        }
 
       } else {
         return
@@ -86,7 +119,56 @@ class Heap {
     }
   }
 
-  private def append(n: Int) = {
+  private def bubbleUp(index: Int = lastIndex): Unit = {
+    require(
+      index < heapRepresentation.length && index >= 0,
+      "heapRepresentation.length = " + heapRepresentation.length +
+      ", index = " + index +
+      ", positions = " + positions)
+    var nodeIndex = index
+
+    def smallerThanParent = {
+      val parentNodeIndex = parentIndex(nodeIndex)
+
+      if (rightChildIndex(parentNodeIndex) == nodeIndex) {
+        if (rightChildIndex(parentNodeIndex) < leftChildIndex(parentNodeIndex)) {
+          nodeIndex = nodeIndex - 1
+
+        } else if (rightChildIndex(parentNodeIndex) > leftChildIndex(parentNodeIndex)) {
+          nodeIndex = nodeIndex
+        }
+      }
+
+      heapRepresentation(nodeIndex) < heapRepresentation(parentNodeIndex)
+    }
+
+    while (parentIndex(nodeIndex) > IndexOfMinValue && smallerThanParent) {
+      val parentNodeIndex = parentIndex(nodeIndex)
+      swap(nodeIndex, parentIndex(nodeIndex))
+
+      if (rightChildIndex(parentNodeIndex) == nodeIndex) {
+        assert(
+          heapRepresentation(parentNodeIndex) <= heapRepresentation(nodeIndex) &&
+          heapRepresentation(parentNodeIndex) <= heapRepresentation(nodeIndex - 1))
+
+      } else if (leftChildIndex(parentNodeIndex) == nodeIndex) {
+        assert(
+          heapRepresentation(parentNodeIndex) <= heapRepresentation(nodeIndex))
+      }
+      nodeIndex = parentNodeIndex
+    }
+
+    if (
+      parentIndex(nodeIndex) == IndexOfMinValue &&
+      nodeIndex != parentIndex(nodeIndex) &&
+      heapRepresentation(nodeIndex) < heapRepresentation(parentIndex(nodeIndex))) {
+
+      swap(nodeIndex, parentIndex(nodeIndex))
+      nodeIndex = parentIndex(nodeIndex)
+    }
+  }
+
+  private def append(n: T) = {
     val positionOfNewElement = positions.getOrElse(n, ArrayBuffer[Int]())
     heapRepresentation += n
 
@@ -98,22 +180,24 @@ class Heap {
     }
   }
 
-  private def removeLastElement() = {
+  private def removeLastElement(): T = {
     val positionOfRemovedElement = positions.getOrElse(heapRepresentation.last, ArrayBuffer[Int]())
+    val lastElement = heapRepresentation.last
 
     if (positionOfRemovedElement.nonEmpty) {
       positionOfRemovedElement -= (lastIndex)
-
-    } else {
-      positions -= heapRepresentation.last
     }
 
     heapRepresentation.remove(lastIndex)
+
+    return lastElement
   }
 
-  private def swap(i: Int, j: Int) = {
+  private def swap(i: Int, j: Int): Unit = {
     require(i < heapRepresentation.length && i >= IndexOfMinValue)
     require(j < heapRepresentation.length && j >= IndexOfMinValue)
+
+    if (i == j) return
 
     val temp = heapRepresentation(i)
 
@@ -124,12 +208,4 @@ class Heap {
     positions(heapRepresentation(i)) += i
     positions(heapRepresentation(j)) += j
   }
-}
-
-object Main extends App {
-  val heap = new Heap
-
-  for (i <- 5 to 1 by -1) heap.insert(i)
-
-  println(heap.content)
 }
--- Heap/src/test/scala/heap/HeapSuite.scala
+++ Heap/src/test/scala/heap/HeapSuite.scala
@@ -1,10 +1,11 @@
 package heap
 
 import org.scalatest.FunSuite
+import collection.mutable.ArrayBuffer
 
 class HeapSuite extends FunSuite {
-  test("min-heap extractMin correctness") {
-    val heap = new Heap
+  test("continuous extractMin of a heap consists of 1 to 5 should return 1 to 5 sequentially") {
+    val heap = new Heap[Int]
 
     for (i <- 5 to 1 by -1) heap.insert(i)
 
@@ -16,27 +17,58 @@ class HeapSuite extends FunSuite {
     intercept[java.lang.IndexOutOfBoundsException](heap.extractMin())
   }
 
-  test("min-heap insert correctness") {
-    val heap = new Heap
+  test("the smaller one of new node and original last node should bubble up") {
+    val heap = new Heap[Int]
 
-    for (i <- 5 to 1 by -1) heap.insert(i)
+    heap.insert(2)
+    heap.insert(1)
+    heap.insert(3)
 
-    assert(
-      heap
-        .content
-        .sameElements(collection.mutable.ArrayBuffer[Int](1, 2, 4, 5, 3))
-    )
+    assert(heap.toArrayBuffer === ArrayBuffer(1, 2, 3))
   }
 
-  test("min-heap delete correctness") {
-    val heap = new Heap
 
-    for (i <- 5 to 1 by -1) heap.insert(i)
 
-    assert(heap.delete(5) === Some(5))
-    assert(heap.content.contains(5) === false)
-    assert(heap.delete(6) === None)
-    assert(heap.delete(1) === Some(1))
-    assert(heap.content.contains(1) === false)
+
+
+  test("the smaller one of remaining nodes should bubble up") {
+    val heap = new Heap[Int]
+
+    heap.insert(1)
+    heap.insert(2)
+    heap.insert(3)
+    heap.delete(1)
+
+    assert(heap.toArrayBuffer === ArrayBuffer(2, 3))
+  }
+
+  test("bubble up correctness in delete process") {
+    val heap = new Heap[Int]
+    val input = List(1, 90, 2, 91, 92, 3, 4, 93, 94, 95, 96, 5)
+    val output = ArrayBuffer(1, 5, 2, 90, 92, 3, 4, 93, 94, 95, 96)
+
+    for (i <- input) heap.insert(i)
+    heap.delete(91)
+
+    assert(heap.toArrayBuffer === output)
+  }
+
+  test("bubble down correctness in delete process") {
+    val heap = new Heap[Int]
+    val input = List(1, 90, 2, 91, 92, 3, 4, 93, 94, 95, 96)
+    val output = ArrayBuffer(1, 91, 2, 93, 92, 3, 4, 96, 94, 95)
+
+    for (i <- input) heap.insert(i)
+    heap.delete(90)
+
+    assert(heap.toArrayBuffer === output)
+  }
+
+  test("delete last element in array representation should execute normally") {
+    val heap = new Heap[Int]
+
+    heap.insert(1)
+    heap.insert(2)
+    heap.delete(2)
   }
 }
