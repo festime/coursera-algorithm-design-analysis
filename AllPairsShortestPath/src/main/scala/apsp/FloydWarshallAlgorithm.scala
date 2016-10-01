524ff7db147b552403aea9014891deee6d8aefb9
Yulin Chen <gloverock2@gmail.com>
2016-10-01 10:46:16 +0800

    Implement Floyd-Warshall algorithm
    遇到 bug
    對每一個 test_input 輸出結果都是同一個負值
    
    debug 思考過程
    1. 檢查初始化的資料是否正確
    2. 演算法是正確的，是自己實作出了問題
    3. 仔細看一遍演算法的虛擬碼
       和自己的實作是否有正確對應
    4. 發現兩個變數參考到同一個陣列，造成操作錯誤
    5. 突然意識到程式輸出結果的負值
       正好是 Int 型態的最小值 Int.MinValue
       也等於 Int.MaxValue + 1
       代表哪邊的相加操作可能有整數溢位的問題
       因為程式碼規模很小，很容易就找到了
       造成問題的整數相加運算式
       加上條件判斷後就解決了
    7. 對三個 test_input 的 output 終於不再一樣
       根據題目要求，提交認為的正確答案但錯
    8. 猜測可能是 Int 型態依然不夠容納中途的計算結果
       把型態改為 Long 再執行一遍
       發現對 input_1, input_2 的 output 分別變成
       兩個極小的負值
       但對 input_3 的 output 維持 19 不變
       猜測 input_1, input_2 可能有 negative cycle
       提交答案 19 ，正確
    9. 檢視投影片
       Floyd-Warshall algorithm 偵測 negative cycle 的部分
       發現原來認知有錯誤
       誤實作成 Bellman-Ford algorithm 偵測 negative cycle 的想法
       修改後終於正確回報 negative cycle

---
+++ AllPairsShortestPath/src/main/scala/apsp/FloydWarshallAlgorithm.scala
@@ -0,0 +1,122 @@
+package apsp
+
+import scala.io.Source
+import collection.mutable.{HashMap, ArrayBuffer}
+
+class FloydWarshallAlgorithm(inputFile: String) {
+  val PositiveInfinity = Int.MaxValue
+  val (graph, numVertices, numEdges, initialCost) = {
+    val lineIterator = Source.fromFile(inputFile).getLines
+    val (numVertices, numEdges) =
+      lineIterator.next.split(" ") match {
+        case Array(numVertices, numEdges) =>
+          (numVertices.toInt, numEdges.toInt)
+      }
+    val N = numVertices + 1
+    val graph = ArrayBuffer.fill(N)(HashMap[Int, Int]())
+    val initialCost =
+      ArrayBuffer
+        .fill(N)(ArrayBuffer.fill(N)(PositiveInfinity))
+
+    for (line <- lineIterator) {
+      val (tail, head, cost) = line.split(" ") map (_.toInt) match {
+        case Array(tail, head, cost) => (tail, head, cost)
+      }
+
+      graph(tail)(head) = cost
+      initialCost(tail)(head) = cost
+    }
+
+    for (i <- 1 to numVertices) initialCost(i)(i) = 0
+
+    // assert that number of edges is correct
+    assert((graph map (_.keys.size)).sum == numEdges)
+    // assert that initialCost is correct
+    for (i <- 1 to numVertices) {
+      for (j <- 1 to numVertices) {
+        initialCost(i)(j) match {
+          case PositiveInfinity => assert(graph(i).get(j) == None && i != j)
+          case 0 => assert(i == j || graph(i)(j) == 0)
+          case _ => assert(graph(i).get(j) != None)
+        }
+      }
+    }
+
+    (graph, numVertices, numEdges, initialCost)
+  }
+  val N = numVertices + 1
+
+  def run(): Option[Long] = {
+    var a = initialCost
+    var b = ArrayBuffer.fill(N)(ArrayBuffer.fill(N)(0))
+
+    for (k <- 1 to numVertices) {
+      for (i <- 1 to numVertices) {
+        for (j <- 1 to numVertices) {
+          val costOne = a(i)(j)
+          val costTwo =
+            if (a(i)(k) == PositiveInfinity || a(k)(j) == PositiveInfinity)
+              PositiveInfinity
+            else
+              a(i)(k) + a(k)(j)
+
+          b(i)(j) =
+            if (costOne < costTwo)
+              costOne
+            else
+              costTwo
+        }
+      }
+
+      /*
+       * 重複利用陣列，避免額外的建立和 GC 開銷
+       */
+      var temp = b
+      b = a
+      a = temp
+    }
+
+    /*
+     * 檢查是否有 negative cycle
+     */
+    for (i <- 1 to numVertices) {
+      if (a(i)(i) < 0) {
+        println("Negative cycle!")
+        return None
+      }
+    }
+
+    /*
+     * 找最短的「兩點間最短路徑」長度
+     * 先把最終結果 flatten 再排序，最前面的就是最小的
+     * 比起迭代兩層迴圈找最小值
+     * 算是一個小小優化吧
+     *
+     * 時間複雜度從 O(n ^ 2) 變成 O(n log n)
+     * 再來，這樣避免了 branch prediction 失敗對效能的影響
+     *
+     * 不過還是要再強調一次
+     * 這只是小小優化
+     * 因為上面的過程就是 O(n ^ 3) 了，主要時間花在這上面
+     * 而且若圖有 negative cycle
+     * 也不會執行到這裡
+     */
+    val minCost = a.flatten.sorted.head
+
+//    for (i <- 1 to numVertices) {
+//      for (j <- 1 to numVertices) {
+//        if (a(i)(j) < minCost)
+//          minCost = a(i)(j)
+//      }
+//    }
+
+    println(minCost)
+    return Some(minCost)
+  }
+}
+
+object FloydWarshallAlgorithm extends App {
+  new FloydWarshallAlgorithm("test_input_1.txt").run
+  new FloydWarshallAlgorithm("test_input_2.txt").run
+  new FloydWarshallAlgorithm("test_input_3.txt").run
+}
